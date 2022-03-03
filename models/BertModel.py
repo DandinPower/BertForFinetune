6377e8a18fdae23c2edda6831e062818b4c471a2
DandinPower <t108590045@ntut.org.tw>
2022-03-04 01:06:33 +0800

    add: training logs

--- models/BertModel.py
+++ models/BertModel.py
@@ -1620,8 +1620,8 @@ def train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs,
     Defined in :numref:`sec_image_augmentation`"""
     timer, num_batches = d2l.Timer(), len(train_iter)
     print(num_batches)
-    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0, 1],
-                            legend=['train loss', 'train acc', 'test acc'])
+    #animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0, 1],
+    #                        legend=['train loss', 'train acc', 'test acc'])
     net = nn.DataParallel(net, device_ids=devices).to(devices[0])
     for epoch in range(num_epochs):
         # Sum of training loss, sum of training accuracy, no. of examples,
@@ -1633,12 +1633,14 @@ def train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs,
                 net, features, labels, loss, trainer, devices)
             metric.add(l, acc, labels.shape[0], labels.numel())
             timer.stop()
-            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
-                animator.add(epoch + (i + 1) / num_batches,
-                             (metric[0] / metric[2], metric[1] / metric[3],
-                              None))
+            #if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                #animator.add(epoch + (i + 1) / num_batches,
+                             #(metric[0] / metric[2], metric[1] / metric[3],
+                              #None))
         test_acc = d2l.evaluate_accuracy_gpu(net, test_iter)
-        animator.add(epoch + 1, (None, None, test_acc))
+        #animator.add(epoch + 1, (None, None, test_acc))
+        print(f'loss {metric[0] / metric[2]:.3f}, train acc '
+          f'{metric[1] / metric[3]:.3f}, test acc {test_acc:.3f}')
     print(f'loss {metric[0] / metric[2]:.3f}, train acc '
           f'{metric[1] / metric[3]:.3f}, test acc {test_acc:.3f}')
     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec on '
